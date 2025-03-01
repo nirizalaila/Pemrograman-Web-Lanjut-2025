be4d2e27948046bfce2bd9c9a21a1d6b8126df95
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-03-01 22:34:42 +0700

    Praktikum 4

--- Minggu 3/PWL_POS/routes/web.php
+++ Minggu 3/PWL_POS/routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +14,7 @@ use App\Http\Controllers\SalesController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage'])->name('category.food-beverage');
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth'])->name('category.beauty-health');
-    Route::get('/home-care', [ProductController::class, 'homeCare'])->name('category.home-care');
-    Route::get('/baby-kid', [ProductController::class, 'babyKid'])->name('category.baby-kid');
+Route::get('/', function() {
+    return view('welcome');
 });
-
-Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('user.show');
-
-Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
+Route::get('/level', [LevelController::class, 'index']);
