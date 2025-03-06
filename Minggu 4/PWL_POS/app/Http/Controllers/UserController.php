090e65f1cd3307e3a69a99644d5668b8412cb28b
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-03-06 11:54:11 +0700

    Praktikum 1 - $fillable

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -11,10 +11,20 @@ class UserController extends Controller
     public function index()
     {
         //tambah data user dengan Eloquent Model
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
         $data = [
-            'nama' => 'Pelanggan Pertama'
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        UserModel::create($data);
 
         //coba akses mode UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
