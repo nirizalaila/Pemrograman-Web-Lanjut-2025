a1b7d3cbc13a0319b9546a11bddcb7ffed0add90
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-03-06 12:11:44 +0700

    Praktikum 2.1 - Retrieving Single Models

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -18,16 +18,27 @@ class UserController extends Controller
         //     'password' => Hash::make('12345')
         // ];
         // UserModel::create($data);
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        //coba akses mode UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(1,['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+        $user = UserModel::findOr(20,['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
