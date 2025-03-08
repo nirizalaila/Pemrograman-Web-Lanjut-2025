6a9c4596a5aa8d2a67069b2599054413dd6d181c
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-03-09 01:50:41 +0700

    Praktikum 2.5 - Attribute Changes

--- Minggu 4/PWL_POS/app/Models/UserModel.php
+++ Minggu 4/PWL_POS/app/Models/UserModel.php
@@ -13,5 +13,5 @@ class UserModel extends Model
     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
     
     // protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
