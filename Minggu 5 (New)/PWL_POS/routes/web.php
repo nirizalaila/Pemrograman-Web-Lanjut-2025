7705449048aae2ceaca200bf62cb1116bc596afe
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-03-25 00:23:02 +0700

    Tugas 1 - Implementasi Tabel m_level

--- Minggu 5 (New)/PWL_POS/routes/web.php
+++ Minggu 5 (New)/PWL_POS/routes/web.php
@@ -36,3 +36,13 @@ Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
 Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
 Route::put('/{id}', [UserController::class, 'update']); // menyimpan data user yang diubah
 Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+Route::group(['prefix' => 'level'], function(){
+    Route::get('/', [LevelController::class, 'index']); // menampilkan halaman awal level
+    Route::post('/list', [LevelController::class, 'list']); // menampilkan data level dalam bentuk json untuk datables
+    Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah level
+    Route::post('/', [LevelController::class, 'store']); // menyimpan data level baru
+    Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail level
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit level
+    Route::put('/{id}', [LevelController::class, 'update']); // menyimpan data level yang diubah
+    Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data level
+});
