3f68bf4f84a58ba99dffeeb3a7c4f13d5fadbd5d
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-04-27 13:56:13 +0700

    Tugas - Implementasi API untuk upload file/gambar pada tabel m_barang

--- Minggu 11/PWL_POS/app/Http/Controllers/Api/BarangController.php
+++ Minggu 11/PWL_POS/app/Http/Controllers/Api/BarangController.php
@@ -4,17 +4,54 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator; 
 use App\Models\BarangModel;
 
 class BarangController extends Controller
 {
     public function index() {
-        return BarangModel::all();
+        $barangs = BarangModel::all();
+        return response()->json($barangs);
     }
 
     public function store(Request $request) {
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        // Set validation
+        $validator = Validator::make($request->all(), [
+            'kategori_id' => 'required',
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Tambahkan validasi image
+        ]);
+        
+        // If validations fail
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create barang
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $request->image->hashName(), // Simpan nama file image yang sudah di-hash
+        ]);
+
+        // Return response JSON barang is created
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+
+        // Return JSON if process failed
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $barang) {
--- Minggu 11/PWL_POS/app/Models/BarangModel.php
+++ Minggu 11/PWL_POS/app/Models/BarangModel.php
@@ -1,9 +1,10 @@
 <?php
  
  namespace App\Models;
- 
- use Illuminate\Database\Eloquent\Factories\HasFactory;
- use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
  
  class BarangModel extends Model
  {
@@ -21,10 +22,17 @@
          'barang_kode',
          'barang_nama',
          'harga_beli',
-         'harga_jual'
+         'harga_jual',
+         'image' //tambahan
      ];
  
      public function kategori() {
          return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
      }
+
+     protected function image(): Attribute {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' .$image),
+        );
+    }
  }
